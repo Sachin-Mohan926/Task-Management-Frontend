e2a7c8581fbb0385f8e34159b8423bb9e7623351
Sachin-Mohan926 <mohansachin7766@gmail.com>
2025-02-23 02:40:46 +0530

    navbar updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,9 +8,9 @@ const Navbar = ({ currUser, handleLogout }) => {
             <span>Hello {currUser.fullName}</span>
 
             <Link to="/">Home</Link>
-            <Link to="/login">Login</Link>
+            {/* <Link to="/login">Login</Link> */}
             <button onClick={handleLogout}>Log Out</button>
-            <Link to="/sign-up">Sign up</Link>
+            {/* <Link to="/sign-up">Sign up</Link> */}
             <Link to='/tasks'>Tasks</Link>
         </div>
     );
