17266d858bc8f6b5a03abec1afbbc2e451d21ab2
Sachin-Mohan926 <mohansachin7766@gmail.com>
2025-02-23 13:02:55 +0530

    resolving task issues

--- src/components/taskList.jsx
+++ src/components/taskList.jsx
@@ -65,7 +65,6 @@ const TaskList = ({ list, getData, filterObj, title }) => {
             }),
             headers: {
                 "content-type": "application/json",
-                "Authorization": `Bearer ${localStorage.getItem("token")}`,
             },
         });
         const respObj = await resp.json();
